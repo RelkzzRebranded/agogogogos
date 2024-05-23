62c0b39d4d3509ccd59751739b8bb957157f5cb9
RelkzzRebranded <130754062+RelkzzRebranded@users.noreply.github.com>
2024-05-23 17:07:11 +0800

    NEWAIMBOT WITH PREDICTION.lua

--- AIMBOT WITH PREDICTION.lua
+++ AIMBOT WITH PREDICTION.lua
@@ -196,12 +196,12 @@ local CHEAT_CLIENT = {} do
 		CHEAT_CLIENT.camera = game.Workspace.CurrentCamera
 		CHEAT_CLIENT.mouse = CHEAT_CLIENT.player:GetMouse()
 	end
-
+ 
 	do
 		function CHEAT_CLIENT:get_target()
 			local current_target = nil
 			local maximum_distance = math.huge
-
+	
 			for i,v in pairs(game.Players:GetPlayers()) do
 				if v == game.Players.LocalPlayer then continue end
 				if v.Character and v.Character:FindFirstChild("HumanoidRootPart") then
@@ -213,39 +213,60 @@ local CHEAT_CLIENT = {} do
 					maximum_distance = distance
 				end
 			end
-
+	
 			return current_target
 		end
-
+	
 		function CHEAT_CLIENT:predict_velocity(target_part, projectile_speed)
 				local velocity = target_part.Velocity
-
+	
 				local distance = (CHEAT_CLIENT.camera.CFrame.p - target_part.CFrame.p).Magnitude
 				local time_to_hit = (distance / projectile_speed)
-
+	
 				local predicted_position = target_part.CFrame.p + (velocity * time_to_hit)
 				local delta = (predicted_position - target_part.CFrame.p).Magnitude
-
+	
 				local final_projectile_speed = projectile_speed - 0.013 * projectile_speed ^ 2 * time_to_hit ^ 2
-
+	
 				time_to_hit += (delta / final_projectile_speed)
-
+	
 				return target_part.CFrame.p + (velocity * time_to_hit)
 		end
-
+	
 		function CHEAT_CLIENT:predict_drop(target_part, projectile_speed, projectile_drop)
 				local distance = (CHEAT_CLIENT.camera.CFrame.p - target_part.CFrame.p).Magnitude
 				local time_to_hit = (distance / projectile_speed)
-
+	
 				local final_projectile_speed = projectile_speed - 0.013 * projectile_speed ^ 2 * time_to_hit ^ 2
 				time_to_hit += (distance / final_projectile_speed)
-
+	
 				local drop_timing = projectile_drop * time_to_hit ^ 2
-
+	
 				if not tostring(drop_timing):find("nan") then
 					return drop_timing
 				end
 				return 0
+		end
+	
+		function CHEAT_CLIENT:get_bullet_atributes()
+			local attribute_value = nil
+			local status = game.ReplicatedStorage.Players[CHEAT_CLIENT.player.Name]:FindFirstChild("Status")
+			if status then
+				local equipped_tool = status.GameplayVariables.EquippedTool.Value
+				if equipped_tool then
+					local inventory_equipped_tool = game.ReplicatedStorage.Players[CHEAT_CLIENT.player.Name].Inventory:FindFirstChild(tostring(equipped_tool))
+					if inventory_equipped_tool then
+						local mag = inventory_equipped_tool.Attachments:FindFirstChild("Magazine") and inventory_equipped_tool.Attachments:FindFirstChild("Magazine"):FindFirstChildOfClass("StringValue") and inventory_equipped_tool.Attachments:FindFirstChild("Magazine"):FindFirstChildOfClass("StringValue"):FindFirstChild("ItemProperties").LoadedAmmo or inventory_equipped_tool.ItemProperties:FindFirstChild("LoadedAmmo")
+						if mag then
+							local first_bullet_type = mag:FindFirstChild("1")
+							if first_bullet_type then
+								attribute_value = game.ReplicatedStorage.AmmoTypes[first_bullet_type:GetAttribute("AmmoType")]:GetAttributes()
+							end
+						end
+					end
+				end
+			end
+			return attribute_value
 		end
 
 		function CHEAT_CLIENT:get_bullet_atribute(attribute)
@@ -268,8 +289,35 @@ local CHEAT_CLIENT = {} do
 			end
 			return attribute_value
 		end
+
+		function CHEAT_CLIENT:set_bullet_atribute(attribute, val)
+			local attribute_value = nil
+			local status = game.ReplicatedStorage.Players[CHEAT_CLIENT.player.Name]:FindFirstChild("Status")
+			if status then
+				local equipped_tool = status.GameplayVariables.EquippedTool.Value
+				if equipped_tool then
+					local inventory_equipped_tool = game.ReplicatedStorage.Players[CHEAT_CLIENT.player.Name].Inventory:FindFirstChild(tostring(equipped_tool))
+					if inventory_equipped_tool then
+						local mag = inventory_equipped_tool.Attachments:FindFirstChild("Magazine") and inventory_equipped_tool.Attachments:FindFirstChild("Magazine"):FindFirstChildOfClass("StringValue") and inventory_equipped_tool.Attachments:FindFirstChild("Magazine"):FindFirstChildOfClass("StringValue"):FindFirstChild("ItemProperties").LoadedAmmo or inventory_equipped_tool.ItemProperties:FindFirstChild("LoadedAmmo")
+						if mag then
+							for _,bbbbb in next, mag:GetChildren() do
+								attribute_value = game.ReplicatedStorage.AmmoTypes[bbbbb:GetAttribute("AmmoType")]:SetAttribute(attribute, val)
+							end
+						end
+					end
+				end
+			end
+			return attribute_value
+		end
 	end
  end
+
+ coroutine.wrap(function()
+	while task.wait(1) do
+		CHEAT_CLIENT:set_bullet_atribute('RecoilStrength', 0)
+		CHEAT_CLIENT:set_bullet_atribute('ProjectileDrop', 0)
+	end
+end)()
 
 local function Load()
 	ServiceConnections.RenderSteppedConnection = RunService.RenderStepped:Connect(function()
@@ -446,10 +494,10 @@ if Environment.Settings.ReloadOnTeleport then
 	if queueonteleport then
 		queueonteleport(game:HttpGet("https://raw.githubusercontent.com/RelkzzRebranded/agogogogos/main/AIMBOT%20WITH%20PREDICTION.lua"))
 	else
-		SendNotification(Title, "Your exploit does not support \"syn.queue_on_teleport()\"")
+		SendNotification(Title, "Your exploit does not support queueonteleport")
 	end
 end
 
 --// Load
 
-Load(); SendNotification(Title, "Aimbot script successfully loaded! Check the GitHub page on how to configure the script.", 5)
+Load(); SendNotification(Title, "Aimbot script successfully loaded!", 5)
