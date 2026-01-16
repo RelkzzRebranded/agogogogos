a23aa9c4cf3e38d0dd02e3ba6892eaa6feef598e
RelkzzRebranded <130754062+RelkzzRebranded@users.noreply.github.com>
2026-01-17 03:40:45 +0800

    CHANGE HAMMER MINIGAME METHOD

--- BAREBONES-AUTO-FORGE.lua
+++ BAREBONES-AUTO-FORGE.lua
@@ -1,56 +1,123 @@
 -- THIS IS BAREBONES SO EXPECT SHIT TO NOT BE PERFECT
+local Player = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local RunService = game:GetService("RunService")
 local Controllers = ReplicatedStorage:WaitForChild("Controllers", 30)
 local ForgeController = Controllers:WaitForChild("ForgeController", 30)
 local MeltMini = ForgeController:WaitForChild("MeltMinigame", 30)
 local PourMini = ForgeController:WaitForChild("PourMinigame", 30)
-local HammerMini = ForgeController:WaitForChild("HammerMinigame", 30)
 
+local LocalPlayer = Player.LocalPlayer
+local PlayerGui = LocalPlayer.PlayerGui
+
+local ForgeApp = PlayerGui:WaitForChild("Forge", 30)
+local HammerMiniLayout = ForgeApp:WaitForChild("HammerMinigame", 30) 
 local Debris = workspace:WaitForChild("Debris", 30)
 
 -- Initialized Modules
 local InitMelt = require(MeltMini)
 local InitPour = require(PourMini)
-local InitHammer = require(HammerMini)
 
 -- Variables
 local OldMelt
 local OldPour
-local OldHammer
 
 -- Replacements
 -- Replacement functions for hooks
 
-local MiniReplacement = function()
+local MiniReplacement = function(...)
 	return workspace:GetServerTimeNow()
 end
 
-local NoteReplacement = function(...)
-	local args = {...}
-	local data = args[2]
-  -- i suck at making things perfect so pray that you'll get a 100% masterwork
-	local pp = workspace:GetServerTimeNow() + data.Lifetime * 25 / 44
+-- Math 
+local function nearlyEqual(a, b, tolerance)
+	return math.abs(a - b) <= tolerance
+end
 
-	task.wait(math.max(pp - workspace:GetServerTimeNow(), 0))
-	
-	return true
+local function UDimNearlyEqual(a, b, tolerance)
+	return nearlyEqual(a.Scale, b.Scale, tolerance)
+end
+
+local function UDim2NearlyEqual(a, b, tolerance)
+	return UDimNearlyEqual(a.X, b.X, tolerance)
+		and UDimNearlyEqual(a.Y, b.Y, tolerance)
+end
+
+-- Manager
+
+-- get it? because the minigame is basically osu itself
+local OSUManager = {
+	Buttons = {}
+}
+
+function OSUManager:Add(button)
+	if button:IsA("TextButton") then
+		table.insert(self.Buttons, button)
+	end
+end
+
+function OSUManager:Remove(button)
+	local t = self.Buttons
+	for i = 1, #t do
+		if t[i] == button then
+			t[i] = t[#t]
+			t[#t] = nil
+			break
+		end
+	end
+end
+
+function OSUManager:Pulse()
+	local validButtons = {}
+	local tIdx = 0
+
+	for i = 1, #self.Buttons do
+        local t = self.Buttons[i]
+        if t.Parent == HammerMiniLayout then
+            tIdx = tIdx + 1
+            validButtons[tIdx] = t
+        end
+    end
+	local clicked = {}
+	for i = 1, tIdx do
+		local main = validButtons[i]
+		local frame = main.Frame
+		local circle = frame.Circle
+		if not clicked[main] and (circle.Size.X.Scale <= frame.Size.X.Scale and circle.Size.Y.Scale <= frame.Size.Y.Scale) then
+			firesignal(main.MouseButton1Click)
+			clicked[main] = true
+		end
+	end
 end
 
 -- Init
 OldMelt = hookfunction(rawget(InitMelt, "Start"), MiniReplacement)
 OldPour = hookfunction(rawget(InitPour, "Start"), MiniReplacement)
-OldNote = hookfunction(rawget(InitHammer, "CreateNote"), NoteReplacement)
 
+HammerMiniLayout.ChildAdded:Connect(function(inst)
+	OSUManager:Add(inst)
+end)
+
+HammerMiniLayout.ChildRemoved:Connect(function(inst)
+	if inst:IsA("TextButton") then
+		OSUManager:Remove(inst)
+	end
+end)
 
 Debris.ChildAdded:Connect(function(instance)
-	if instance.Name == "Mold" then
+	if instance.Name == "Mold" and instance:IsA("Model") then
 		local clicker = instance:FindFirstChildOfClass("ClickDetector")
-		if not clicker then return end
-		repeat
-			task.wait(1/8) -- 8 Hz gaming XDDDDD
+		while clicker.Parent do
+			task.wait(0.05)
 			fireclickdetector(clicker)
-		until not instance:IsDescendantOf(workspace)
+		end
 	end
+end)
+
+local last = 0
+RunService.RenderStepped:Connect(function()
+	if tick() - last < 1/24 then return end
+	last = tick()
+	OSUManager:Pulse()
 end)
 
